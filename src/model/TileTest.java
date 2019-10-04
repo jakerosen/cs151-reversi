c5e503bad683932d1b456c7b57713e1d5acc23d2
jakerosen <jakesrosen@gmail.com>
2019-10-04 13:13:14 -0700

    Fixed TileTest to work with the changes to Tile.

--- src/model/TileTest.java
+++ src/model/TileTest.java
@@ -9,20 +9,20 @@ class TileTest {
 	@Test
 	@SuppressWarnings("unlikely-arg-type")
 	void tileTest() {
-		Tile tile1 = new Tile(null);
+		Tile tile1 = new Tile(0,0);
 		assertEquals(Color.EMPTY, tile1.getState());
 		assertEquals(0, tile1.hashCode());
-		
+
 		tile1.setState(Color.BLACK);
 		assertEquals(Color.BLACK, tile1.getState());
 		assertEquals(1, tile1.hashCode());
-		
-		Tile tile2 = new Tile(null);
+
+		Tile tile2 = new Tile(0,0);
 		tile2.setState(Color.WHITE);
 		assertEquals(Color.WHITE, tile2.getState());
 		assertEquals(2, tile2.hashCode());
-		
-		Tile tile3 = new Tile(null);
+
+		Tile tile3 = new Tile(0,0);
 		assertTrue(tile3.equals(tile3));
 		assertFalse(tile1.equals(tile2));
 		assertFalse(tile1.equals(tile3));
@@ -33,7 +33,7 @@ class TileTest {
 		assertFalse(tile1.hashCode() == tile2.hashCode());
 		assertFalse(tile1.hashCode() == tile3.hashCode());
 		assertFalse(tile2.hashCode() == tile3.hashCode());
-		
+
 		tile3.setState(Color.BLACK);
 		assertTrue(tile3.equals(tile3));
 		assertTrue(tile3.equals(tile1));
@@ -42,7 +42,7 @@ class TileTest {
 		assertFalse(tile2.equals(tile3));
 		assertTrue(tile1.hashCode() == tile3.hashCode());
 		assertFalse(tile2.hashCode() == tile3.hashCode());
-		
+
 		tile3.setState(Color.WHITE);
 		assertTrue(tile3.equals(tile3));
 		assertTrue(tile3.equals(tile2));
@@ -51,7 +51,7 @@ class TileTest {
 		assertFalse(tile1.equals(tile3));
 		assertTrue(tile2.hashCode() == tile3.hashCode());
 		assertFalse(tile1.hashCode() == tile3.hashCode());
-		
+
 		tile3.setState(Color.EMPTY);
 		String notATile = null;
 		assertFalse(tile1.equals(notATile));
