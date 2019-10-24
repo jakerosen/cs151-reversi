b25f2c08c9a2d3fd1d2dcf5a3bf68f3c925cba8c
Kevin R. <kwrotunni@gmail.com>
2019-10-24 10:14:00 -0700

    Implemented playPiece in all its spaghetti-y glory

--- src/model/Board.java
+++ src/model/Board.java
@@ -103,11 +103,41 @@ public class Board {
   /**
    * Places a piece on the Tile in the given location of the color of the given player,
    * and flips co-linear Tiles accordingly
-   * @param location Which Tile to play on
+   * @param x x coord of Tile to play on
+   * @param y y coord of Tile to play on
    * @param player Which player to play for
    */
-  public void playPiece(int location, Color player) {
-
+  public void playPiece(int x, int y, Color player) {
+    boolean player1Turn = player == Color.BLACK;
+    int playerNum = (player1Turn)? 1:2; //for later hacky stuff
+    LinkedList<Tile> playerPieces = (player1Turn)? blacks:whites;
+    LinkedList<Tile> otherPieces = (player1Turn)? whites:blacks;
+    Tile t = tiles[x][y];
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        if (i != 0 || j != 0) {
+          int x1 = t.getX() + i;
+          int y1 = t.getY() + j;
+          if(x1 >= 0 && x1 <= 7 && y1 >= 0 && y1 <= 7) {
+            Tile t1 = tiles[x1][y1];
+            if (2*t1.getStateNumeric()%3 == playerNum) {
+              while (x1 > 0 && x1 < 7 && y1 > 0 && y1 < 7
+                      && 2*t1.getStateNumeric()%3 == playerNum) {
+                t1 = tiles[x1 += i][y1 += j];
+              }
+              if (t1.getState() == player) {
+                while (x1 != x && y1 != y)
+                {
+                  (t1 = tiles[x1 -= i][y1 -= j]).setState(player);
+                  otherPieces.remove(t1);
+                  if (!playerPieces.contains(t1)) playerPieces.add(t1);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   /**
